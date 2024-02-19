56abe61520a48a7293f2c42501941f35d46467dd
Germán <germanmoreyra94@gmail.com>
2024-02-19 23:35:08 +0100

    Add missing summary

--- BookInterview/Attributes/CheckBooksEmptyAttribute.cs
+++ BookInterview/Attributes/CheckBooksEmptyAttribute.cs
@@ -4,8 +4,16 @@ using Data.Entities;
 
 namespace Api.Attributes;
 
+/// <summary>
+/// This attribute checks if the result of an action is an empty list of books.
+/// If it is, it changes the result to a NotFoundResult.
+/// </summary>
 public class CheckBooksEmptyAttribute : ActionFilterAttribute
 {
+    /// <summary>
+    /// This method is called after an action has been executed.
+    /// </summary>
+    /// <param name="context">The context of the action.</param>
     public override void OnActionExecuted(ActionExecutedContext context)
     {
         if (context.Result is ObjectResult objectResult && objectResult.Value is IEnumerable<BookEntity> books && !books.Any())
