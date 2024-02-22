d4c0e3b310739ad03ca16b36f8e0d2f06aaaee4e
Germán <germanmoreyra94@gmail.com>
2024-02-22 12:04:20 +0100

    Chore: transformation of the `BookDto` class into a `record` and the modification of its properties from `set` to `init`.

--- Application/DTOs/BookDto.cs
+++ Application/DTOs/BookDto.cs
@@ -3,35 +3,35 @@
 /// <summary>
 /// Data Transfer Object for Book
 /// </summary>
-public class BookDto
+public record BookDto
 {
     /// <summary>
     /// Gets or sets the author of the book.
     /// </summary>
-    public string? Author { get; set; }
+    public string? Author { get; init; }
 
     /// <summary>
     /// Gets or sets the description of the book.
     /// </summary>
-    public string? Description { get; set; }
+    public string? Description { get; init; }
 
     /// <summary>
     /// Gets or sets the title of the book.
     /// </summary>
-    public string? Title { get; set; }
+    public string? Title { get; init; }
 
     /// <summary>
     /// Gets or sets the genre of the book.
     /// </summary>
-    public string? Genre { get; set; }
+    public string? Genre { get; init; }
 
     /// <summary>
     /// Gets or sets the price of the book.
     /// </summary>
-    public double? Price { get; set; }
+    public double? Price { get; init; }
 
     /// <summary>
     /// Gets or sets the publish date of the book.
     /// </summary>
-    public DateTime? PublishDate { get; set; }
+    public DateTime? PublishDate { get; init; }
 }
