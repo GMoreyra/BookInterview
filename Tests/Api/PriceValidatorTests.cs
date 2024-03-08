56a8a59aa63a4b4ddd531ff2dceda64f2819cda6
Germán <germanmoreyra94@gmail.com>
2024-03-08 16:56:00 +0100

    Add more tests cases

--- Tests/Api/PriceValidatorTests.cs
+++ Tests/Api/PriceValidatorTests.cs
@@ -7,9 +7,14 @@ public class PriceValidatorTests
 {
     [Theory]
     [InlineData(-1.0, null, "Minimum price must be positive.")]
+    [InlineData(-1.0, 10.0, "Minimum price must be positive.")]
     [InlineData(null, -1.0, "Maximum price must be positive.")]
+    [InlineData(10.0, -1.0, "Maximum price must be positive.")]
     [InlineData(10.0, 5.0, "Minimum price cannot be greater than maximum price.")]
     [InlineData(5.0, 5.0, null)]
+    [InlineData(0.0, null, null)]
+    [InlineData(null, 0.0, null)]
+    [InlineData(0.0, 0.0, null)]
     [InlineData(null, null, null)]
     public void ValidatePrices_ReturnsExpectedResult(double? minPrice, double? maxPrice, string? expected)
     {
